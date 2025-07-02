7f1c8385fc595313ea38b0bf006776277dbb67af
comprecomseguro <elaineamancio986@gmail.com>
2025-07-01 23:23:08 -0300

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ctqxpmqrxpluanmjdcxj.supabase.co";
+const supabaseUrl = "https://onfcxnxuapdzgqhbghmy.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0cXhwbXFyeHBsdWFubWpkY3hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2MTI0MDksImV4cCI6MjA2NDE4ODQwOX0.gKFtBJqKL9VxK9N67vGIF5MwtzoMr0wX7ThzZi5dMMA";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uZmN4bnh1YXBkemdxaGJnaG15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5MDU0MDMsImV4cCI6MjA2NjQ4MTQwM30.bF4dK3QtxRVpBAVXJd0CdKspnfvBOb-VHjhYiB_4mwM";
 export const supabase = createClient(supabaseUrl, supabaseKey);
